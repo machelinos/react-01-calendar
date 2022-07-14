eb1e4fb15cfe75d1e0e6652ec2a40c55ea6a9c28
Marcel Cabrera <marcel.cabrera@gmail.com>
2022-07-14 01:41:04 -0500

    Redux serializable check (not best practice)

--- src/store/store.js
+++ src/store/store.js
@@ -5,5 +5,11 @@ import { uiSlice, calendarSlice } from './';
         reducer: {
             ui: uiSlice.reducer,
             calendar: calendarSlice.reducer
-        }
+        },
+        middleware: (getDefaultMiddleware) => getDefaultMiddleware(
+            {
+                // Pobably should change logic on the Dates properties that are being saved to redux so this check should not be set to false
+                serializableCheck: false
+            }
+        )
     })
